4ee6842cc28f94032f04f2492058ea3a302d4bbb
Josselin Caer <josselin.caer@gmail.com>
2023-10-06 19:38:21 -0300

    Updated login email

--- app/pages/api/auth/[...nextauth].ts
+++ app/pages/api/auth/[...nextauth].ts
@@ -1,54 +1,89 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
 import DiscordProvider from "next-auth/providers/discord";
 import EmailProvider from "next-auth/providers/email";
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
-import Providers from "next-auth/providers";
 import { DynamoDBAdapter } from "@next-auth/dynamodb-adapter";
-import { readFileSync } from "fs";
-import path from "path";
+import { createTransport } from "nodemailer";
 
-import nodemailer from "nodemailer";
+async function quebraSendVerificationRequest(params: any) {
+  const { identifier, url, provider, theme } = params;
+  const { host } = new URL(url);
+  // NOTE: You are not required to use `nodemailer`, use whatever you want.
+  const transport = createTransport(provider.server);
+  const result = await transport.sendMail({
+    to: identifier,
+    from: provider.from,
+    subject: `✨ Lien de connection ${host} `,
+    text: text({ url, host }),
+    html: html({ url, host, theme }),
+  });
+  const failed = result.rejected.concat(result.pending).filter(Boolean);
+  if (failed.length) {
+    throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`);
+  }
+}
 
-// const emailsDir = path.resolve(process.cwd(), "emails");
+/**
+ * Email HTML body
+ * Insert invisible space into domains from being turned into a hyperlink by email
+ * clients like Outlook and Apple mail, as this is confusing because it seems
+ * like they are supposed to click on it to sign in.
+ *
+ * @note We don't add the email address to avoid needing to escape it, if you do, remember to sanitize it!
+ */
+function html(params: { url: string; host: string; theme: any }) {
+  const { url, host, theme } = params;
 
-// const sendVerificationRequest = () => {
-//   console.log("sendVerificationRequest");
+  const escapedHost = host.replace(/\./g, "&#8203;.");
 
-//   // const emailFile = readFileSync(path.join(emailsDir, 'confirm-email.html'), {
-//   //   encoding: 'utf8',
-//   // });
-//   // const emailTemplate = Handlebars.compile(emailFile);
-//   transporter.sendMail({
-//     from: `"⚡ Magic NextAuth" ${process.env.EMAIL_FROM}`,
-//     to: "josselin.caer@gmail.com",
-//     subject: "Your sign-in link for Magic NextAuth",
-//     html: `<p>sendVerificationRequest</p>`,
-//   });
-// };
+  const brandColor = "#62258e";
+  const color = {
+    background: "#f9f9f9",
+    text: "#444",
+    mainBackground: "#fff",
+    buttonBackground: brandColor,
+    buttonBorder: brandColor,
+    buttonText: "#fff",
+  };
 
-// const sendWelcomeEmail = async () => {
-//   console.log("sendWelcomeEmail");
+  return `
+<body style="background: ${color.background};">
+  <table width="100%" border="0" cellspacing="20" cellpadding="0"
+    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
+    <tr>
+      <td align="center"
+        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        Voilà votre lien temporaire pour vous connecter à <strong>${escapedHost}</strong>
+      </td> 
+    </tr>
+    <tr>
+      <td align="center" style="padding: 20px 0;">
+        <table border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${url}"
+                target="_blank"
+                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Se connecter</a></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center"
+        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        Si vous n'avez pas demandé cet e-mail, vous pouvez l'ignorer en toute sécurité.
+      </td>
+    </tr>
+  </table>
+</body>
+`;
+}
 
-//   // const { email } = user;
-
-//   try {
-//     // const emailFile = readFileSync(path.join(emailsDir, 'welcome.html'), {
-//     //   encoding: 'utf8',
-//     // });
-//     // const emailTemplate = Handlebars.compile(emailFile);
-//     await transporter.sendMail({
-//       from: `"⚡ Magic NextAuth" ${process.env.EMAIL_FROM}`,
-//       to: "josselin.caer@gmail.com",
-//       subject: "Welcome to Magic NextAuth! 🎉",
-//       html: `<p>sendWelcomeEmail</p>`,
-//     });
-//   } catch (error) {
-//     console.log(`❌ Unable to send welcome email to user (${"email"})`);
-//   }
-// };
+/** Email Text body (fallback for email clients that don't render HTML, e.g. feature phones) */
+function text({ url, host }: { url: string; host: string }) {
+  return `Sign in to ${host}\n${url}\n\n`;
+}
 
 const config: any = {
   credentials: {
@@ -68,7 +103,6 @@ const client = DynamoDBDocument.from(new DynamoDB(config), {
 
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
-  // events: { createUser: sendWelcomeEmail },
   providers: [
     // GithubProvider({
     //   clientId: process.env.GITHUB_ID as string,
@@ -96,11 +130,23 @@ export const authOptions: NextAuthOptions = {
         },
       },
       from: process.env.EMAIL_FROM,
+      sendVerificationRequest({
+        identifier: email,
+        url,
+        provider: { server, from },
+      }) {
+        quebraSendVerificationRequest({
+          identifier: email,
+          url,
+          provider: { server, from },
+        });
+      },
     }),
   ],
   adapter: DynamoDBAdapter(client),
   pages: {
     signIn: "/auth/signin",
+    verifyRequest: "/auth/verify-request",
   },
   theme: {
     colorScheme: "auto",
