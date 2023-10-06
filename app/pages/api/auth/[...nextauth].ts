2f27fb0f54f6dc2d1894b20309e15a0df6aed548
Josselin Caer <josselin.caer@gmail.com>
2023-10-06 16:52:54 -0300

    Added magic link login

--- app/pages/api/auth/[...nextauth].ts
+++ app/pages/api/auth/[...nextauth].ts
@@ -2,10 +2,53 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import DiscordProvider from "next-auth/providers/discord";
+import EmailProvider from "next-auth/providers/email";
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 import Providers from "next-auth/providers";
 import { DynamoDBAdapter } from "@next-auth/dynamodb-adapter";
+import { readFileSync } from "fs";
+import path from "path";
+
+import nodemailer from "nodemailer";
+
+// const emailsDir = path.resolve(process.cwd(), "emails");
+
+// const sendVerificationRequest = () => {
+//   console.log("sendVerificationRequest");
+
+//   // const emailFile = readFileSync(path.join(emailsDir, 'confirm-email.html'), {
+//   //   encoding: 'utf8',
+//   // });
+//   // const emailTemplate = Handlebars.compile(emailFile);
+//   transporter.sendMail({
+//     from: `"⚡ Magic NextAuth" ${process.env.EMAIL_FROM}`,
+//     to: "josselin.caer@gmail.com",
+//     subject: "Your sign-in link for Magic NextAuth",
+//     html: `<p>sendVerificationRequest</p>`,
+//   });
+// };
+
+// const sendWelcomeEmail = async () => {
+//   console.log("sendWelcomeEmail");
+
+//   // const { email } = user;
+
+//   try {
+//     // const emailFile = readFileSync(path.join(emailsDir, 'welcome.html'), {
+//     //   encoding: 'utf8',
+//     // });
+//     // const emailTemplate = Handlebars.compile(emailFile);
+//     await transporter.sendMail({
+//       from: `"⚡ Magic NextAuth" ${process.env.EMAIL_FROM}`,
+//       to: "josselin.caer@gmail.com",
+//       subject: "Welcome to Magic NextAuth! 🎉",
+//       html: `<p>sendWelcomeEmail</p>`,
+//     });
+//   } catch (error) {
+//     console.log(`❌ Unable to send welcome email to user (${"email"})`);
+//   }
+// };
 
 const config: any = {
   credentials: {
@@ -25,6 +68,7 @@ const client = DynamoDBDocument.from(new DynamoDB(config), {
 
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
+  // events: { createUser: sendWelcomeEmail },
   providers: [
     // GithubProvider({
     //   clientId: process.env.GITHUB_ID as string,
@@ -39,6 +83,20 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
       authorization: { params: { scope: ["identify", "email"].join(" ") } },
     }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        tls: {
+          rejectUnauthorized: false,
+        },
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
   ],
   adapter: DynamoDBAdapter(client),
   pages: {
@@ -52,7 +110,7 @@ export const authOptions: NextAuthOptions = {
   },
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-      // console.log("callbacks signin", user, account, profile);
+      console.log("callbacks signin", user, account, profile);
 
       const isAllowedToSignIn = true;
       if (isAllowedToSignIn) {
