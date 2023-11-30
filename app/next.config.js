0a9a71b58290f192ef4eff9620ff058cc1790a54
Josselin Caer <josselin.caer@gmail.com>
2023-11-30 19:31:15 -0300

    Updated to remotePatterns

--- app/next.config.js
+++ app/next.config.js
@@ -2,11 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      `${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`,
-      `${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com`,
-      `hauteoldassets.blob.core.windows.net`,
-      `cdn.sanity.io`,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: `${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`,
+        // port: '',
+        // pathname: '/account123/**',
+      },
+      {
+        protocol: "https",
+        hostname: `${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com`,
+      },
+      {
+        protocol: "https",
+        hostname: `hauteoldassets.blob.core.windows.net`,
+      },
+      {
+        protocol: "https",
+        hostname: `cdn.sanity.io`,
+      },
     ],
   },
   // i18n: {
